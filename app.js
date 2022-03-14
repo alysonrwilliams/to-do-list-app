5ec5ecf302475e730f564909533b0682882bfe6a
Alyson Williams <alysonrwilliams@gmail.com>
2022-03-13 20:08:16 -0400

    added function to filter out completed tasks

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 //event listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('change', filterTodo);
 
 
 //functions
@@ -25,7 +27,7 @@ function addTodo(event) {
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
 
-    //check mark button
+    //trash can button
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
     trashButton.classList.add("trash-btn");
@@ -40,7 +42,11 @@ function deleteCheck(e) {
     //DELETE TODO
     if(item.classList[0] === 'trash-btn') {
         const todo = item.parentElement;
+        //animation
         todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
     }
 
     //COMPLETE TODO (checkmark)
@@ -49,6 +55,23 @@ function deleteCheck(e) {
         todo.classList.toggle('completed');
     }
     
+}
+
+function filterTodo(e) {
+    const todos = document.querySelectorAll(".todo")
+    todos.forEach(function(todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+                case "completed":
+                    if (todo.classList.contains("completed")) {
+                        todo.style.display = "flex";
+                    } else {
+                        todo.style.display = "none";
+                    }
+        }
+    });
 }
 
 
